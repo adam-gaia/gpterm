73254f7c118c318aa4fcd99535a4f5c548756cb1
Adam Gaia <git@agaia.dev>
2023-05-10 19:50:51 -0600

    feat: flake support

---
+++ devenv.nix
@@ -0,0 +1,19 @@
+{ inputs
+, pkgs
+, lib
+, treefmt-nix
+, ...
+}: {
+  packages = with pkgs; [
+    rust-bin.nightly.latest.default
+    clippy
+    rust-analyzer
+    cargo-nextest
+    cargo-limit
+    cargo-audit
+    cargo-watch
+    cargo-expand
+    just
+    (treefmt-nix.lib.mkWrapper pkgs (import ./treefmt.nix))
+  ];
+}
